096b809f30d5adf1faef30085d0ef2f4d7382480
Bakiera <kar.bakiera@gmail.com>
2022-03-31 18:32:51 +0200

    gcp_main an order changes.

--- {{cookiecutter.repo_name}}/gcp_main.sh
+++ {{cookiecutter.repo_name}}/gcp_main.sh
@@ -1,4 +1,3 @@
-kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/download/v1.10.0/cluster-operator.yml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_log_pvc.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_service.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/pgbouncer.yaml
@@ -6,8 +5,7 @@ kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_roles.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_secret.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/RabbitmqCluster.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_scheduler_deployment.yaml
-kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_webserver_deployment.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_worker_deployment.yaml
-
+kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_webserver_deployment.yaml
 
 
