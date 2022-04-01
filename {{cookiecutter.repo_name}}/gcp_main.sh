9b629a448b7ff919dda76c9328276892cf2c7efa
Bakiera <kar.bakiera@gmail.com>
2022-04-01 10:00:38 +0200

    rabbitmq changes

--- {{cookiecutter.repo_name}}/gcp_main.sh
+++ {{cookiecutter.repo_name}}/gcp_main.sh
@@ -1,3 +1,4 @@
+kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/download/v1.10.0/cluster-operator.yml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_log_pvc.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/airflow_service.yaml
 kubectl apply -f ./{{cookiecutter.yamles_folder_name}}/pgbouncer.yaml
